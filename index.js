ab90de92cfb4431c587f7f291e0b4f1d44a32d35
RincyRaveendran <rincyjiyash@gmail.com>
2024-09-06 20:59:14 +0530

    final

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server')
+const middleware = jsonServer.defaults()
+const router = jsonServer.router("db.json")
+//server
+const employeeServer = jsonServer.create()
+employeeServer.use(middleware)
+employeeServer.use(router)
+
+const PORT = 4000 ||process.env.PORT
+
+employeeServer.listen(PORT,()=>{
+    console.log(`server running successfully at ${PORT}`);
+    
+})
